dd74ded1dfa8706a5732397bf2f0531b76cd514c
Alex Ovcharenko <d.flaeron@gmail.com>
2019-02-01 23:12:46 +0200

    Updates

---
+++ precache-manifest.0d311d46831636752813b80fa215e8b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "08cde64bfc2003ab201b",
+    "url": "/qr-reader-pwa/static/js/runtime~main.08cde64b.js"
+  },
+  {
+    "revision": "66a1d5cccdbe59939472",
+    "url": "/qr-reader-pwa/static/js/main.66a1d5cc.chunk.js"
+  },
+  {
+    "revision": "ab632e6ab0b616c93d1b",
+    "url": "/qr-reader-pwa/static/js/1.ab632e6a.chunk.js"
+  },
+  {
+    "revision": "66a1d5cccdbe59939472",
+    "url": "/qr-reader-pwa/static/css/main.05558f37.chunk.css"
+  },
+  {
+    "revision": "ba0482bd36c06b9aa4b55568b0f0c6ff",
+    "url": "/qr-reader-pwa/index.html"
+  }
+];
